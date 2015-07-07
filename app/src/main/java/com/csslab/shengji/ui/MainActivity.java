3f0fc35f237f2cb263500001e432d447547f643c
czctt2080 <czctt2080@gmail.com>
2015-07-07 16:24:02 +0800

    czc change

--- app/src/main/java/com/csslab/shengji/ui/MainActivity.java
+++ app/src/main/java/com/csslab/shengji/ui/MainActivity.java
@@ -3,7 +3,6 @@ package com.csslab.shengji.ui;
 import android.app.Activity;
 import android.os.Bundle;
 
-import game.csulab.com.ui.R;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 }

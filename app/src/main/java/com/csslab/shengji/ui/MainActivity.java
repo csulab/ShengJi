5dbf6ecd74d2523283e4eb4118f1584c498066b3
czctt2080 <czctt2080@gmail.com>
2015-07-07 17:05:07 +0800

    czc change

--- app/src/main/java/com/csslab/shengji/ui/MainActivity.java
+++ app/src/main/java/com/csslab/shengji/ui/MainActivity.java
@@ -1,7 +1,10 @@
 package com.csslab.shengji.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -10,6 +13,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_test = (Button) findViewById(R.id.btn_test);
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 

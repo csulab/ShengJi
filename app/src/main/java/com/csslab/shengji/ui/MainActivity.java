8ea6937b57b6088e383b27186e80ec5346353b6d
czctt2080 <czctt2080@gmail.com>
2015-07-07 15:53:30 +0800

    czc change

---
+++ app/src/main/java/com/csslab/shengji/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.csslab.shengji.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import game.csulab.com.ui.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
